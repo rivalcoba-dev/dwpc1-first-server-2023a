f3b0db9b75894efb2af2a557d6c4208f92d7b95b
Ivan Rivalcoba <90455420+rivalcoba-dev@users.noreply.github.com>
2024-03-11 17:43:00 +0000

    ⭐ FEAT: index.js Crea el primer servidor basico con el moduloe http

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hola Node!!! 🔥")
+// 1. Importar el modulo http
+import { log } from 'console';
+import http from 'http'
+
+// 2. Creando el servidor y su logica
+// callback => Funcion
+const server = http.createServer( (req, res)=>{
+    // Codigo de comportamiento
+    console.log("> 📢 Se ha recibido una petición");
+    // Respondiendo al cliente
+    res.write("Hola desde el server...");
+    // Terminando la conexion
+    res.end();
+} );
+
+// 3. Poner a trabajar el servidor
+server.listen(3000,"0.0.0.0", (err)=>{
+    // Verificando que no haya error
+    if(err) console.log("❌ Error al iniciar el server");
+    // Si no hay error
+    console.log("🥳 Servidor escuchando en http://localhost:3000");
+});
