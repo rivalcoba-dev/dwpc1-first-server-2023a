4a6c5645019347aad2f13389ea011755399bc9d6
Ivan Rivalcoba <90455420+rivalcoba-dev@users.noreply.github.com>
2024-03-13 16:47:14 +0000

    ⭐ Enrutado de peticiones LTS Agrega favicon interno y ruta author

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // 1. Importar el modulo http
 import http from 'http';
 import path from 'path';
+import { promises as fs } from 'fs';
 
 global["__dirname"] = path.dirname(new URL(import.meta.url).pathname);
 global["__filename"] = path.basename(new URL(import.meta.url).pathname);
@@ -8,7 +9,7 @@ global["__filename"] = path.basename(new URL(import.meta.url).pathname);
 
 // 2. Creando el servidor y su logica
 // callback => Funcion
-const server = http.createServer( (req, res)=>{
+const server = http.createServer( async (req, res)=>{
     // Desestructurando de "req"
     let { url, method } = req;
 
@@ -26,9 +27,9 @@ const server = http.createServer( (req, res)=>{
                 <head>
                 <link 
                     rel="icon" 
-                    type="image/png" 
+                    type="image/x-icon" 
                     sizes="32x32" 
-                    href="https://img.icons8.com/fluency/256/domain.png">
+                    href="/favicon.ico">
                 <title>My App</title>
                 </head>
                 <body> 
@@ -43,7 +44,74 @@ const server = http.createServer( (req, res)=>{
             // Cerrando la comunicacion
             res.end();
             break;
-    
+        case '/author':
+            // Peticion raiz
+            // Estableciendo cabeceras
+            res.setHeader('Content-Type', 'text/html');
+            let url_image = 'https://media.istockphoto.com/id/180841365/photo/hes-a-handsome-man.jpg?s=612x612&w=0&k=20&c=vjQLLI8g_a0O6_xx0plUu3CJ9AMhnSzHssLwgem8gE4=';
+            // Escribiendo la respuesta
+            res.write(`
+            <html>
+                <head>
+                <link 
+                    rel="icon" 
+                    type="image/x-icon" 
+                    sizes="32x32" 
+                    href="/favicon.ico">
+                <title>My App</title>
+                </head>
+                <body style="text-align: center;">
+                <h1 style="color: #333;">&#9889; Author &#9889;</h1>
+                <p style="color: #34495E;">Ivan Rivalcoba Rivas - Web Developer</p>
+                <div>
+                    <img width="300px" src="https://media.istockphoto.com/id/180841365/photo/hes-a-handsome-man.jpg?s=612x612&w=0&k=20&c=vjQLLI8g_a0O6_xx0plUu3CJ9AMhnSzHssLwgem8gE4=" alt="Foto Ivan Rivalcoba">
+                </div>
+                </body>
+            </html>
+            `);
+            console.log(`📣 Respondiendo: 200 ${url} ${method}`);
+            // Estableciendo codigo de respuesta
+            res.statusCode = 200;
+            // Cerrando la comunicacion
+            res.end();
+            break;
+        case "/favicon.ico":
+            // Especificar la ubicación del archivo de icono
+            const faviconPath = path.join(__dirname, 'favicon.ico');
+            try{
+                const data = await fs.readFile(faviconPath);
+                res.writeHead(200, {'Content-Type': 'image/x-icon'});
+                res.end(data);
+            }catch (err) {
+                console.error(err);
+                // Peticion raiz
+                // Estableciendo cabeceras
+                res.setHeader('Content-Type', 'text/html');
+                // Escribiendo la respuesta
+                res.write(`
+                <html>
+                <head>
+                    <link 
+                        rel="icon" 
+                        type="image/png" 
+                        sizes="32x32" 
+                        href="https://img.icons8.com/fluency/256/domain.png">
+                    <title>My App</title>
+                </head>
+                <body> 
+                    <h1>&#128534; 500 El server esta fuera de servicio</h1>
+                    <p>Lo sentimos pero hubo un error en nuestro server...</p>
+                    <p> ${err.message}</p>
+                </body>
+                </html>
+                `);
+                console.log(`📣 Respondiendo: 500 ${req.url} ${req.method}`);
+                // Estableciendo codigo de respuesta
+                res.statusCode = 500;
+                // Cerrando la comunicacion
+                res.end();
+            }
+            break
         default:
             // Estableciendo ruta no encontrada
             res.setHeader('Content-Type', 'text/html');
@@ -53,9 +121,9 @@ const server = http.createServer( (req, res)=>{
                 <head>
                 <link 
                     rel="icon" 
-                    type="image/png" 
+                    type="image/x-icon" 
                     sizes="32x32" 
-                    href="https://img.icons8.com/fluency/256/domain.png">
+                    href="/favicon.ico">
                 <title>My App</title>
                 </head>
                 <body> 
